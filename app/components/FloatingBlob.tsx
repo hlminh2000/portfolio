370d270c2b4b60b854924d2263a19e007e3b6b5b
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2025-01-02 17:22:36 -0500

    handles touch with blob

--- app/components/FloatingBlob.tsx
+++ app/components/FloatingBlob.tsx
@@ -9,8 +9,15 @@ export const FloadingBlob = ( ) => {
     const onMouseMove = (e: MouseEvent) => {
       current.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
     }
+    const onTouchMove = (e: TouchEvent) => {
+      current.style.transform = `translate3d(calc(${e.touches.item(0)?.clientX}px - 50%), calc(${e.touches.item(0)?.clientY}px - 50%), 0)`
+    }
     document.addEventListener('mousemove', onMouseMove)
-    return () => document.removeEventListener('mousemove', onMouseMove)
+    document.addEventListener('touchmove', onTouchMove)
+    return () => {
+      document.removeEventListener('mousemove', onMouseMove)
+      document.removeEventListener('touchmove', onTouchMove)
+    }
   }, [])
   return <div id="blob" ref={blobRef} />
 }
