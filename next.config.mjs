c793d7a414f4fc02730552f040b3d399649dbe78
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2025-01-01 19:52:22 -0500

    ignores eslint

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,13 @@ const nextConfig = {
     )
     return config
   },
+  
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
 
 };
 
