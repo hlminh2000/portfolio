f41469736f6f8f44ca31b14cec5e0b0359e8a832
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2025-01-05 16:22:03 -0500

    implements suspense streaming on blog page

--- app/blog/articles/[slug]/page.tsx
+++ app/blog/articles/[slug]/page.tsx
@@ -4,8 +4,29 @@ import Image from "next/image"
 import { getArticleBySlug } from "../../getSortedPostsData"
 import { Suspense } from "react"
 import { notFound } from "next/navigation"
-import Head from "next/head"
 import { Metadata, ResolvingMetadata } from "next"
+
+export async function generateMetadata(
+  { params, searchParams }: {
+    params: Promise<{ slug: string }>
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const { slug } = await params
+
+  const articleMeta = await getArticleBySlug(slug)
+  if (!articleMeta) return notFound()
+
+  return {
+    title: `Minh Ha | ${articleMeta.title}`,
+    description: articleMeta.preview,
+    openGraph: {
+      images: [...articleMeta.image ? [articleMeta.image.src] : []],
+    },
+  }
+}
 
 const ArticleMetaDisplay = async ({ slug }: { slug: string }) => {
   const articleMeta = await getArticleBySlug(slug)
@@ -23,26 +44,10 @@ const ArticleMetaDisplay = async ({ slug }: { slug: string }) => {
   )
 }
 
-export async function generateMetadata(
-  { params, searchParams }: {
-    params: Promise<{ slug: string }>
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-  },
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
-  const { slug } = await params
-  
-  const articleMeta = await getArticleBySlug(slug)
-  if (!articleMeta) return notFound()
-
-  return {
-    title: `Minh Ha | ${articleMeta.title}`,
-    description: articleMeta.preview,
-    openGraph: {
-      images: [...articleMeta.image ? [articleMeta.image.src] : []],
-    },
-  }
+const Post = async ({ slug }: { slug: string }) => {
+  const PostComponent = (await import(`../${slug}/post.mdx`).catch(err => null))?.default
+  if (!PostComponent) return notFound()
+  return <PostComponent />
 }
 
 export default async function ({
@@ -53,21 +58,20 @@ export default async function ({
 
   const { slug } = await params
 
-  const Post = (await import(`../${slug}/post.mdx`).catch(err => null))?.default
-  if (!Post) return notFound()
-
-  const articleMeta = await getArticleBySlug(slug)
-  if (!articleMeta) return notFound()
-
   return (
     <article className="max-w-3xl mx-auto px-6 prose prose-invert mt-24 relative">
-      <Head>
-        <title>{articleMeta.title}</title>
-        <meta name="description" content={articleMeta.preview} />
-      </Head>
       <div className="rounded-lg bg-gray-900/70 md:p-8 sm:p-4 p-2">
-        <ArticleMetaDisplay slug={slug} />
-        <Post />
+        <Suspense fallback={
+          <>
+            <div className="bg-gray-800/50 text-4xl font-bold mb-8 h-14" />
+            <div className="bg-gray-800/50 text-blue-600 mb-4 h-7" />
+            <div className="bg-gray-800/50 mb-4 h-7" />
+            <div className="bg-gray-800/50 rounded-lg object-cover w-full h-96 bg-center drop-shadow-lg" />
+          </>
+        }>
+          <ArticleMetaDisplay slug={slug} />
+          <Post slug={slug} />
+        </Suspense>
       </div>
     </article>
   )
