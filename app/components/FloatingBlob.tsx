2ebb43837bf7259a2cce24da354ae18b227cd6ca
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2025-01-07 07:43:22 -0500

    switches to used top and left rather than transform

--- app/components/FloatingBlob.tsx
+++ app/components/FloatingBlob.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { isMobile } from 'react-device-detect';
 import { useEffect, useRef } from "react"
 import _ from "lodash"
 
@@ -7,18 +8,24 @@ export const FloatingBlob = ( ) => {
   useEffect(() => {
     const current = blobRef.current
     if (!current) return
-    const onMouseMove = _.throttle((e: MouseEvent) => {
-      current.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-    }, 100)
-    const onTouchMove = _.throttle((e: TouchEvent) => {
-      current.style.transform = `translate3d(calc(${e.touches.item(0)?.clientX}px - 50%), calc(${e.touches.item(0)?.clientY}px - 50%), 0)`
-    }, 100)
-    document.addEventListener('mousemove', onMouseMove)
-    document.addEventListener('touchmove', onTouchMove)
-    return () => {
-      document.removeEventListener('mousemove', onMouseMove)
-      document.removeEventListener('touchmove', onTouchMove)
+    console.log("isMobile: ", isMobile)
+    if (isMobile) {
+      const onTouchMove = _.throttle((e: TouchEvent) => {
+        // current.style.transform = `translate3d(calc(${e.touches.item(0)?.clientX}px - 50%), calc(${e.touches.item(0)?.clientY}px - 50%), 0)`
+        current.style.left = `${e.touches.item(0)?.clientX}px`
+        current.style.top = `${e.touches.item(0)?.clientY}px`
+      }, 100)
+      return () => document.removeEventListener('touchmove', onTouchMove)
+    } else {
+      const onMouseMove = _.throttle((e: MouseEvent) => {
+        // current.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+        current.style.left = `${e.clientX}px`
+        current.style.top = `${e.clientY}px`
+      }, 100)
+      document.addEventListener('mousemove', onMouseMove)
+      return () => document.removeEventListener('mousemove', onMouseMove)
     }
   }, [])
+
   return <div id="blob" ref={blobRef} />
 }
