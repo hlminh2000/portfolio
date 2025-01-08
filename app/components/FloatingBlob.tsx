3e7fab61f8200c00487fd3888212bbe69070294a
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2025-01-08 16:49:41 -0500

    fixes fizbuzz article syntax highlighting

--- app/components/FloatingBlob.tsx
+++ app/components/FloatingBlob.tsx
@@ -9,21 +9,19 @@ export const FloatingBlob = ( ) => {
   useEffect(() => {
     const current = blobRef.current
     if (!current) return
-    if (isMobile) {
-      const onTouchMove = _.throttle((e: TouchEvent) => {
-        // current.style.transform = `translate3d(calc(${e.touches.item(0)?.clientX}px - 50%), calc(${e.touches.item(0)?.clientY}px - 50%), 0)`
-        current.style.left = `${e.touches.item(0)?.clientX}px`
-        current.style.top = `${e.touches.item(0)?.clientY}px`
-      }, 100)
-      return () => document.removeEventListener('touchmove', onTouchMove)
-    } else {
-      const onMouseMove = _.throttle((e: MouseEvent) => {
-        // current.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-        current.style.left = `${e.clientX}px`
-        current.style.top = `${e.clientY}px`
-      }, 100)
-      document.addEventListener('mousemove', onMouseMove)
-      return () => document.removeEventListener('mousemove', onMouseMove)
+    const onTouchMove = _.throttle((e: TouchEvent) => {
+      current.style.left = `${e.touches.item(0)?.clientX}px`
+      current.style.top = `${e.touches.item(0)?.clientY}px`
+    }, 100)
+    const onMouseMove = _.throttle((e: MouseEvent) => {
+      current.style.left = `${e.clientX}px`
+      current.style.top = `${e.clientY}px`
+    }, 100)
+    document.addEventListener('touchmove', onTouchMove)
+    document.addEventListener('mousemove', onMouseMove)
+    return () => {
+      document.removeEventListener('touchmove', onTouchMove)
+      document.removeEventListener('mousemove', onMouseMove)
     }
   }, [])
 
