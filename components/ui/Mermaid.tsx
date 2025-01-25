0f1010ad3f365ae24faa805aeacff0f641b79f23
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2025-01-24 20:36:51 -0500

    another post

--- components/ui/Mermaid.tsx
+++ components/ui/Mermaid.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, { useEffect } from "react";
 import mermaid from "mermaid";
+import { Expand } from "lucide-react";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./dialog";
 
 mermaid.initialize({
   startOnLoad: true,
@@ -9,18 +11,35 @@ mermaid.initialize({
   fontFamily: "geistMono"
 });
 
-// export default class Mermaid extends React.Component {
-//   componentDidMount() {
-//     mermaid.contentLoaded();
-//   }
-//   render() {
-//     return <div className="mermaid">{this.props.chart}</div>;
-//   }
-// }
-export const Mermaid = ({chart}: {chart: string}) => {
+const Diagram = ({ chart }: { chart: string }) => {
   useEffect(() => {
-    console.log("chart: ", chart)
     mermaid.contentLoaded();
   }, [])
-  return <div className="mermaid">{chart}</div>;
+  return (
+    <div className="mermaid max-h-full flex justify-center">
+      {chart}
+    </div>
+  )
+}
+
+export const Mermaid = ({ chart }: { chart: string }) => {
+  useEffect(() => {
+    mermaid.contentLoaded();
+  }, [])
+  return (
+    <div>
+      <div className="flex justify-end">
+
+        <Dialog>
+          <DialogTrigger>
+            <Expand className="h-4 w-4" />
+          </DialogTrigger>
+          <DialogContent className="max-w-6xl h-5/6 my-10 p-0 flex flex-col align-middle justify-center">
+            <Diagram chart={chart} />
+          </DialogContent>
+        </Dialog>
+      </div>
+      <Diagram chart={chart} />
+    </div>
+  );
 }
